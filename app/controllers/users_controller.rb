ab5b61cce90dc727107f66f77e52dc1581863a3b
Samantha Rabling <sjarillorabling@gmail.com>
2018-04-26 10:14:22 -0700

    working on htmls and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name:params[:name], alias:params[:alias], email:params[:email], password:params[:password], password_confirm:params[:confirm])
+    @user = User.new(name:params[:name], alias:params[:alias], email:params[:email], password:params[:password], password_confirmation:params[:confirm])
     if @user.save
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect_to "/bright_ideas"
     else
       flash[:errors] = @user.errors.full_messages
@@ -14,5 +14,6 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
